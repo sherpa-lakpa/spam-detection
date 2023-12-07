545b9d19836177b6a1edabe8d1a6a867288578b4
Lakpa Sherpa <Lakpa Sherpa>
2023-12-06 22:49:13 -0500

    comment removed

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import joblib
 import string
-import nltk
 from nltk.corpus import stopwords
 
 # function to remove punctuation and stopwords
@@ -13,9 +12,7 @@ def text_process(text):
     non_punc=''.join(non_punc)
     return [word for word in non_punc.split() if word not in stopwords.words('english')]
 
-# Load the pre-trained model and vectorizer
-model = joblib.load('spam_model.pkl')
-vectorizer = joblib.load('vectorizer.pkl')
+# Load the pre-trained model
 pipe_spam_model = joblib.load('pipe_spam_model.pkl')
 
 # Create Flask app
@@ -33,11 +30,6 @@ def predict():
         # Get the input text from the user
         user_input = [request.form['email_text']]
 
-        # Vectorize the input text using the pre-trained vectorizer
-        input_vector = vectorizer.transform(user_input)
-
-        # Make a prediction using the pre-trained model
-        prediction = model.predict(input_vector)
         prediction = pipe_spam_model.predict(user_input)
         print(prediction[0])
         # Display the result on the result page
